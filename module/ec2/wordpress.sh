47d8fa3757e9499624877716987d60fee793312d
rajangold02 <rajangold02@gmail.com>
2018-08-28 13:03:00 -0400

    final changes

--- module/ec2/wordpress.sh
+++ module/ec2/wordpress.sh
@@ -8,6 +8,5 @@ sudo wget http://wordpress.org/latest.tar.gz
 sudo tar -xvf latest.tar.gz
 cd wordpress
 sudo cp -r * .. && cd ..
-touch health.txt
-echo "I'm Health" >health.txt
+sudo touch health.txt
 sudo service httpd restart
