3659652aa17973ab20e27527028f6ef98636a0a5
rajangold02 <rajangold02@gmail.com>
2018-08-28 09:13:22 -0400

    changes

--- module/elb/main.tf
+++ module/elb/main.tf
@@ -2,7 +2,6 @@ resource "aws_elb" "example" {
   name               = "${var.name}"
   subnets            = ["${var.subnet_id1}"]
   security_groups    = ["${aws_security_group.elb.id}"]
-  availability_zones = ["us-east-1a", "us-east-1b"]
 
   listener {
     instance_port     = 80
